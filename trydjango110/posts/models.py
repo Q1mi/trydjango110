361ea0d2c415629c485f3523897d86f2b8ec4066
Q1mi <liwenzhou7@gmail.com>
2017-01-04 23:05:18 +0800

    update day04

---
+++ trydjango110/posts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+# MVC框架
+# Model View Controllers
+# MTV <-> Django
+# Model View Template
+
+# 跟数据库相关的
+
+# 帖子
+class Posts(models.Model):
+
+    title = models.CharField(max_length=256)  # 标题，存文字的
+    content = models.TextField()  # 内容
+    update = models.DateTimeField(auto_now=True, auto_now_add=False)  # 更新的时间戳
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)  # 创建时的时间戳
+
+    def __str__():  # Python3
+        return self.title
+
+    def __unicode__():  # Python2
+        return self.title
