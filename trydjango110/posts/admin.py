c822c3875bc2374fb0e116a38f1e2f7bf865852b
Q1mi <liwenzhou7@gmail.com>
2017-01-10 00:04:25 +0800

    update to No.6

--- trydjango110/posts/admin.py
+++ trydjango110/posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from posts import models
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "update", "timestamp"]
+    list_display_links = ["content",]
+    list_filter = ["timestamp", "content"]
+    search_fields = ["title", "content"]
+    list_editable = ["title", ]
+
+    class Meta:
+        model = models.Post
+
+# 一行代码实现model在admin中可见
+admin.site.register(models.Post, PostAdmin)
