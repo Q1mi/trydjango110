be35aa9db078520152716dc4f5702998fa96249e
Q1mi <liwenzhou7@gmail.com>
2017-04-29 22:28:41 +0800

    add static_assets

--- bbs/settings.py
+++ bbs/settings.py
@@ -123,5 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "statics"),
+    os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_assets")
